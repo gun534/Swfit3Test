4d132e790828471cf8addf327ce16fd8cbf5f562
gun534 <gun534@yahoo.co.jp>
2017-01-26 00:53:05 +0900

    Initial Commit

---
+++ Swfit3Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swfit3Test
+//
+//  Created by 前田 雅紀 on 2017/01/26.
+//  Copyright © 2017年 crossphase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
